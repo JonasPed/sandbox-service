68fa2e4b7b5258f49ffef97327c80f8f2f7d189a
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-08-17 14:57:42 +0200

    Push to jonaspkit.

--- documentation/docker/getOldDoc.sh
+++ documentation/docker/getOldDoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if docker pull kvalitetsit/sandbox-service-documentation:latest; then
+if docker pull jonaspkit/sandbox-service-documentation:latest; then
     echo "Copy from old documentation image."
     docker cp $(docker create kvalitetsit/sandbox-service-documentation:latest):/usr/share/nginx/html target/old
 fi
