456a7ea7e38c01c41787a837083943b9e3b28a0f
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-08-17 14:41:07 +0200

    Rename

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-![Build Status](https://github.com/KvalitetsIT/kithugs/workflows/CICD/badge.svg)
-# kithugs
+![Build Status](https://github.com/KvalitetsIT/sandbox-service/workflows/CICD/badge.svg)
+# sandbox-service
 
 Template repository showing how to be a good Java Spring Boot citizen in a k8s cluster.
 
--- build/failOnFirstBuild.sh
+++ build/failOnFirstBuild.sh
@@ -2,7 +2,7 @@
 
 echo "${GITHUB_REPOSITORY}"
 echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/kithugs" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kithugs" ]; then
+if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/sandbox-service" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/sandbox-service" ]; then
   echo "Please run setup.sh REPOSITORY_NAME"
   exit 1
 fi
